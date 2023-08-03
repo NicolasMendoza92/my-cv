46553cbfcbd5b8645e432c613fa85747b92547a2
NicoMen <nicomendoza.92@gmail.com>
2023-08-03 14:50:56 +0200

    se cambio unos links

--- src/components/Cursos.jsx
+++ src/components/Cursos.jsx
@@ -26,11 +26,11 @@ const Cursos = () => {
         if (newWindow) newWindow.opener = null
     }
     const ecommerceFront = () => {
-        const newWindow = window.open('ecommerce-front-rust.vercel.app');
+        const newWindow = window.open('https://ecommerce-front-rust.vercel.app/');
         if (newWindow) newWindow.opener = null
     }
     const ecommerceAdmin = () => {
-        const newWindow = window.open('ecommerce-admin-delta-lemon.vercel.app');
+        const newWindow = window.open('https://ecommerce-admin-delta-lemon.vercel.app/');
         if (newWindow) newWindow.opener = null
     }
 
@@ -39,64 +39,52 @@ const Cursos = () => {
         <div data-aos="fade-up" className="div-projects">
             <h3 className='text-center m-3' style={{ color:'#0d6efd' }}>My Projects </h3>
             <div  className='row'>
-
-                <div className="col-6  col-lg-4 d-flex justify-content-center">
-                    <div onClick={goToCoco} className="card bg-dark text-white m-2" style={{ width: '25rem', border: 'none' }}>
+                <div className="col 12 col-md-6  col-lg-4 d-flex justify-content-center">
+                    <div onClick={goToCoco} className="card bg-dark text-white m-2" style={{ width: '25rem', height:"12rem",border: 'none' }}>
                         <img src="https://res.cloudinary.com/dbv6dgwez/image/upload/v1669635721/CV/cocomad_project_paqpfc.png" className="card-img" alt="..." />
                         <div className="card-img-overlay">
                             <div className="glassCard">
-                            <h5 className="card-title">React Project</h5>
-                            <p className="card-text">"Ecommerce project"</p>
+                            <h5 className="card-title">Ecommerce project</h5>
+                            <p className="card-text">React JS</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-6 col-lg-4 d-flex justify-content-center">
-                    <div onClick={ecommerceFront} className="card bg-dark text-white m-2" style={{ width: '25rem', border: 'none' }}>
+                <div className="col 12 col-md-6 col-lg-4 d-flex justify-content-center">
+                    <div onClick={ecommerceFront} className="card bg-dark text-white m-2" style={{ width: '25rem',height:"12rem", border: 'none' }}>
                         <img src="https://res.cloudinary.com/dbv6dgwez/image/upload/v1691061919/front_ecommerce_xaao8c.png" className="card-img" alt="..." />
                         <div className="card-img-overlay">
                             <div className="glassCard">
-                            <h5 className="card-title">Front ecommerce Project</h5>
+                            <h5 className="card-title">Front ecommerce project</h5>
                             <p className="card-text">Next JS</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-6 col-lg-4 d-flex justify-content-center">
-                    <div onClick={ecommerceAdmin} className="card bg-dark text-white m-2" style={{ width: '25rem', border: 'none' }}>
+                <div className="col 12 col-md-6 col-lg-4 d-flex justify-content-center">
+                    <div onClick={ecommerceAdmin} className="card bg-dark text-white m-2" style={{ width: '25rem',height:"12rem", border: 'none' }}>
                         <img src="https://res.cloudinary.com/dbv6dgwez/image/upload/v1691061779/admin_ecommerce_z6nhs9.png" className="card-img" alt="..." />
                         <div className="card-img-overlay">
                             <div className="glassCard">
-                            <h5 className="card-title">Adminboard Project</h5>
+                            <h5 className="card-title">Adminboard project</h5>
                             <p className="card-text">Next JS</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-12  col-lg-4 d-flex justify-content-center">
-                    <div onClick={goToGit} className="card bg-dark text-white m-2" style={{ width: '25rem', border: 'none' }}>
+                <div className="col 12 col-md-6 col-lg-4 d-flex justify-content-center">
+                    <div onClick={goToGit} className="card bg-dark text-white m-2" style={{ width: '25rem',height:"12rem", border: 'none' }}>
                         <img src="https://res.cloudinary.com/dbv6dgwez/image/upload/v1669635719/CV/coding_computer_tga7ki.jpg" className="card-img" alt="..." />
                         <div className="card-img-overlay">
                             <div className="glassCard">
-                            <h5 className="card-title">My Projects Portfolio</h5>
-                            <p className="card-text">Take a look at my Frontend and Backend projects</p>
+                            <h5 className="card-title">Github projects</h5>
+                            <p className="card-text">Take a look at my frontend and backend projects</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-6 col-lg-4 d-flex justify-content-center">
-                    <div onClick={goToAllcot} className="card bg-dark text-white m-2" style={{ width: '25rem', border: 'none' }}>
-                        <img src="https://res.cloudinary.com/dbv6dgwez/image/upload/v1669635721/CV/allcot_trading_project_jleivq.png" className="card-img" alt="..." />
-                        <div className="card-img-overlay">
-                            <div className="glassCard">
-                            <h5 className="card-title">Wordpress Project</h5>
-                            <p className="card-text">Business page</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-6 col-lg-4 d-flex justify-content-center">
-                    <div onClick={goToRick} className="card bg-dark text-white m-2" style={{ width: '25rem', border: 'none' }}>
+                <div className="col 12 col-md-6 col-lg-4 d-flex justify-content-center">
+                    <div onClick={goToRick} className="card bg-dark text-white m-2" style={{ width: '25rem',height:"12rem", border: 'none' }}>
                         <img src="https://res.cloudinary.com/dbv6dgwez/image/upload/v1684846613/RickMorty/r_m_wallpaper_bukdib.jpg" className="card-img" alt="..." />
                         <div className="card-img-overlay">
                             <div className="glassCard">
@@ -106,6 +94,18 @@ const Cursos = () => {
                         </div>
                     </div>
                 </div>
+                <div className="col 12 col-md-6 col-lg-4 d-flex justify-content-center">
+                    <div onClick={goToAllcot} className="card bg-dark text-white m-2" style={{ width: '25rem',height:"12rem", border: 'none' }}>
+                        <img src="https://res.cloudinary.com/dbv6dgwez/image/upload/v1669635721/CV/allcot_trading_project_jleivq.png" className="card-img" alt="..." />
+                        <div className="card-img-overlay">
+                            <div className="glassCard">
+                            <h5 className="card-title">Business page</h5>
+                            <p className="card-text">Wordpress</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
             </div >
         </div>
     )
